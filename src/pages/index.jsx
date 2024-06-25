427c8e231c005a486fe25badbb4e5fbd6390c2d0
felprangel <feliperangel897@gmail.com>
2024-06-25 20:59:37 -0300

    adiciona cabeçalho na página inicial

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,18 @@
+import Cabecalho from "@/components/Cabecalho"
+import styled from "styled-components"
+
 const Teste = () => {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Cabecalho />
+      <Main>Hello World</Main>
+    </>
+  )
 }
+
+const Main = styled.main`
+  background-color: #F0EEF1;
+  box-shadow: inset 0px 5px 15px -3px rgba(0,0,0,0.1);
+`
 
 export default Teste
