a996a69e6520aa9a8164eac20c5e6dacf61f305f
felprangel <feliperangel897@gmail.com>
2024-06-25 21:03:35 -0300

    indentação

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,22 +1,22 @@
-import styled from "styled-components"
+import styled from 'styled-components'
 
 const Cabecalho = () => {
-    return (
-        <Header>
-            <h1>Biblioteca</h1>
-        </Header>
-    )
+	return (
+		<Header>
+			<h1>Biblioteca</h1>
+		</Header>
+	)
 }
 
 const Header = styled.header`
-    background-color: #FFFBFB;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 7em;
-    h1 {
-        font-size: 2.5em;
-    }
+	background-color: #fffbfb;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 7em;
+	h1 {
+		font-size: 2.5em;
+	}
 `
 
 export default Cabecalho
