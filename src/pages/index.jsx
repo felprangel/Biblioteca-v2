54fd7ca7e30a018679ec6aa4c58206eaba8f03cd
felprangel <feliperangel897@gmail.com>
2024-06-25 21:04:00 -0300

    adiciona estilização ao container

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,25 @@
-import Cabecalho from "@/components/Cabecalho"
-import styled from "styled-components"
+import Cabecalho from '@/components/Cabecalho'
+import styled from 'styled-components'
 
 const Teste = () => {
-  return (
-    <>
-      <Cabecalho />
-      <Main>Hello World</Main>
-    </>
-  )
+	return (
+		<Container>
+			<Cabecalho />
+			<Main>Hello World</Main>
+		</Container>
+	)
 }
 
+const Container = styled.div`
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 15% 1fr;
+	height: 100vh;
+`
+
 const Main = styled.main`
-  background-color: #F0EEF1;
-  box-shadow: inset 0px 5px 15px -3px rgba(0,0,0,0.1);
+	background-color: #f0eef1;
+	box-shadow: inset 0px 5px 15px -3px rgba(0, 0, 0, 0.1);
 `
 
 export default Teste
