6fa6734eb0f64352b5aef0b1105b020a01e8dd14
felprangel <feliperangel897@gmail.com>
2024-06-25 21:01:44 -0300

    altera forma de importar a fonte

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
-
     * {
         margin: 0;
         box-sizing: border-box;
