7a458e21d72ccb63c3ef2e7b51689411d5c94c50
felprangel <feliperangel897@gmail.com>
2024-06-26 21:41:15 -0300

    cria variáveis de cor

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        --cor-sombra: rgba(0, 0, 0, 0.1);
+        --preto: black;
+        --cinza: #F0EEF1;
+        --branco: #FFFBFB;
+    }
+
     * {
         margin: 0;
         box-sizing: border-box;
